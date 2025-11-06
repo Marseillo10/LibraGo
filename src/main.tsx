5de541633d9e87e3638ec8091756c755b8719ee8
Marseillo Satrian <marsellorafaelbintangsatrian@gmail.com>
2025-11-06 22:56:16 +0700

    fix: Change router import to named import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "./context/ThemeContext";
-import router from "./routing/routes";
+import { router } from "./routing/routes";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
